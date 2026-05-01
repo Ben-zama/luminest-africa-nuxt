0f5420192298e54a3ca5ae8ce121e547c0af9ea1
Ben-Zama <benzamasimon@gmail.com>
2026-05-01 22:01:06 +0100

    apikey fix

--- app/pages/onboarding/area.vue
+++ app/pages/onboarding/area.vue
@@ -111,7 +111,7 @@ const loadingCities = ref(false)
 // Reusable fetch options with the Toneflix API Key
 const getApiOptions = () => {
   const headers = new Headers()
-  headers.append("X-Api-Key", config.public.toneflixApiKey)
+  headers.append("X-Api-Key", config.public.apiKey)
   headers.append("Accept", "application/json")
   headers.append("Content-Type", "application/json")
 
